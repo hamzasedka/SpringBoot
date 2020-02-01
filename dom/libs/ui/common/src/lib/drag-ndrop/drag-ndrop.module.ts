3a822c056d5cb8005d8cebe5c67dc000412eeb50
haithem khalifa <haithem-khalifa@forlink-group.com>
2020-02-01 23:31:06 +0100

    Add AppMaterialDesignModule to DragNDropModule

--- dom/libs/ui/common/src/lib/drag-ndrop/drag-ndrop.module.ts
+++ dom/libs/ui/common/src/lib/drag-ndrop/drag-ndrop.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropzoneDirective } from './directives';
 import { UploaderComponent } from './uploader/uploader.component';
+import { AppMaterialDesignModule } from '../material/app-material-design.module';
 
 
 
 @NgModule({
   declarations: [DropzoneDirective, UploaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppMaterialDesignModule
   ],
   exports: [DropzoneDirective, UploaderComponent]
 })
